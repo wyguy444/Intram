5fb93603a4c1f45491e945dfaf23b462094e6dbb
davidsealand <sealand@usc.edu>
2017-06-17 12:31:52 -0700

    run

--- Intram/index.android.js
+++ Intram/index.android.js
@@ -17,10 +17,10 @@ export default class Intram extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome David!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          To get ended, edit index.android.js
         </Text>
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
